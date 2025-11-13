e2ddc977af2b754ac4433b57c6836d9db83cedc0
Lokeshwaran Krishnasamy <lokeshwarankrishnasamy0107@gmail.com>
2025-11-13 21:24:48 +0530

    Add files via upload

---
+++ vaigai.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+print "Content-type: text/html\n\n";
+
+print <<"HTML";
+<html>
+<head>
+<title>SDN vaigai river </title>
+</head>
+<body><h2>The Vaigai is a river in the Tamil Nadu state of southern India; it passes through the towns of Theni, Madurai, Manamadurai, Paramakudi and Ramanathapuram.[2] It originates in Varusanadu Hills, the Periyar Plateau of the Western Ghats range, and flows northeast through the Kambam Valley, which lies between the Palani Hills to the north and the Varushanad Hills to the south. The Vattaparai Falls are located on this river. As it rounds the eastern corner of the Varushanad Hills, the river turns southeast, running through the region of Pandya Nadu. Madurai, the largest city in the Pandya Nadu region and its ancient capital, lies on the Vaigai. The river empties into the Palk Bay near Alagankulam, close to Pamban Bridge in Ramanathapuram District.
+
+</h2></body>
+</html>
+HTML
+exit;
