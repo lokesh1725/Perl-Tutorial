f2d43ba53592bea77d6019daca07ecae4c9c419a
Lokeshwaran Krishnasamy <lokeshwarankrishnasamy0107@gmail.com>
2025-11-13 21:24:20 +0530

    Add files via upload

---
+++ temple.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+print "Content-type: text/html\n\n";
+
+print <<"HTML";
+<html>
+<head>
+
+<title>sdn mariyamman temple</title>
+</head>
+<body><h1> Mariammankovil form sholavandan </h1></body>
+</html>
+HTML
+exit;
